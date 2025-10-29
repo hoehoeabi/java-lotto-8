2f8c74866796778875ae4ab7d005f2fee8d6557f
hoehoeabi <0203jyh00@naver.com>
2025-10-29 17:28:46 +0900

    Feat : 메인 클래스 구현
    콘피그 클래스로부터 의존성 주입이 끝난 컨트롤러를 실행합니다.
    
    도중에 예외가 던져지면 여기서 잡고 에러 메세지를 띄웁니다.
    
    최종적으론 콘솔을 닫아줍니다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,18 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            new AppConfig()
+                    .controller()
+                    .lotteryStart();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            Console.close();
+        }
     }
 }
