0ddbee888b52de9a468e4a481402659dd9e99bc3
hoehoeabi <0203jyh00@naver.com>
2025-10-29 17:10:08 +0900

    Feat : 로또 클래스
    도메인 패키지로 옮겼습니다.(로또 등수 열거형  클래스와 같이 있는게 맞다고 생각했습니다.)
    
    VO이기때문에 게터를 열어두었고 불변객체로 반환하기에 오직 읽기만 가능하게 하였습니다.
    
    보너스 번호 비교를 위한 contains 메서드를 만들었습니다.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,9 @@
-package lotto;
+package lotto.domain;
 
+import java.util.Collections;
 import java.util.List;
+
+import static lotto.config.LottoConstants.LOTTO_NUMBER_COUNT;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +14,17 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
     // TODO: 추가 기능 구현
+    public List<Integer> getNumbers(){
+        return Collections.unmodifiableList(this.numbers);
+    }
+
+    public boolean contains(Integer number) {
+        return numbers.contains(number);
+    }
 }
