b2608dcaec5eb966c07e8da7a8aa0291f5134a3e
hoehoeabi <0203jyh00@naver.com>
2025-10-29 18:51:40 +0900

    Refactor : 검증 클래스에서 메서드 분리 하였습니다.
    총 6개인지
    중복이 없는지
    1~45사이인지
    검증합니다

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -20,18 +20,30 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != LOTTO_NUMBER_COUNT) {
-            throw new IllegalArgumentException(LOTTO_NUMBER_MAX_COUNT_EXCEPTION);
-        }
+        isOverSix(numbers);
 
+        isDuplicateNumber(numbers);
+
+        isOverOrUnderBoundary(numbers);
+    }
+
+    private static void isOverOrUnderBoundary(List<Integer> numbers) {
+        if(numbers.getLast() > MAX_LOTTO_NUMBER
+                || numbers.getFirst() < MIN_LOTTO_NUMBER){
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_BOUNDRY_EXCEPTION);
+        }
+    }
+
+    private static void isDuplicateNumber(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(DUPLICATE_LOTTO_NUMBERS);
         }
+    }
 
-        if(numbers.getLast() > MAX_LOTTO_NUMBER
-                || numbers.getFirst() < MIN_LOTTO_NUMBER){
-            throw new IllegalArgumentException(ErrorMessage.LOTTO_NUMBER_BOUNDRY_EXCEPTION);
+    private static void isOverSix(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
+            throw new IllegalArgumentException(LOTTO_NUMBER_MAX_COUNT_EXCEPTION);
         }
     }
 
