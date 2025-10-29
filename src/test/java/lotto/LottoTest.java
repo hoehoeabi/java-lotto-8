82245a7c19013423fd43d2eca2d8b2e9e8173914
hoehoeabi <0203jyh00@naver.com>
2025-10-29 20:02:24 +0900

    Test : Lotto 단위테스트 작성
    1. Lotto객체의 생성자 속 validate 메서드를 단위테스트를 작성하였습니다.
    
    2. 보너스 숫자가 로또안에 있는지 확인하는 contains메서드 단위테스트를 작성하였습니다.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class LottoTest {
     @Test
@@ -22,4 +24,32 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호에 1~45를 벗어난 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_경계를_벗어난_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void 로또_번호의_개수가_6개가_안되면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("보너스 번호가 로또번호 안에 있으면 true를 반환한다.")
+    @Test
+    void 로또_번호중_보너스번호와_같은_숫자가_있다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.contains(1)).isTrue();
+    }
+
+    @DisplayName("보너스 번호가 로또번호 안에 없으면 false를 반환한다.")
+    @Test
+    void 로또_번호중_보너스번호와_같은_숫자가_없다() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        assertThat(lotto.contains(7)).isFalse();
+    }
+
 }
