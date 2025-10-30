756688cb480211cb9b48a772b1a2ba80348fac06
hoehoeabi <0203jyh00@naver.com>
2025-10-30 16:49:25 +0900

    Feat : 예외 메세지 추가
    1. 보너스 숫자와 입력금액이 0 일경우
    2. 보너스 숫자가 로또 당첨번호와 중복되는 경우
    
    사용하는 예외 메세지입니다.

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -14,9 +14,10 @@ public final class ErrorMessage {
 
     public static final String INVALID_PURCHASE_AMOUNT_UNIT = ERROR_PREFIX + "구입 금액은 " + GameConstants.LOTTO_PRICE + "원 단위여야 합니다.";
     public static final String INVALID_NUMBER_FORMAT = ERROR_PREFIX + "숫자를 입력해야 합니다.";
+    public static final String CANNOT_BE_ZERO = "0은 입력할 수 없습니다.";
     public static final String INVALID_LOTTO_NUMERS_FORMART = ERROR_PREFIX + "양식은 숫자사이에 ,만 와야하며 공백은 불가합니다.";
     public static final String DUPLICATE_LOTTO_NUMBERS = ERROR_PREFIX + "로또 번호는 중복될 수 없습니다.";
     public static final String LOTTO_NUMBER_BOUNDRY_EXCEPTION = ERROR_PREFIX + "숫자는 1~45만 가능합니다";
     public static final String LOTTO_NUMBER_MAX_COUNT_EXCEPTION = ERROR_PREFIX + "로또 번호는 6개여야 합니다.";
-
+    public static final String BONUS_NUMBER_CONTAINS_IN_LOTTO = ERROR_PREFIX + "보너스 번호는 로또 번호에 포함되면 안됩니다.";
 }
