2060e3e120ef30d07f0762d6842f3d4227462efb
hoehoeabi <0203jyh00@naver.com>
2025-10-31 22:53:18 +0900

    Refactor : LottoConstants를 필드에서 삭제 후 정적 변수를 바로 불러오도록 하였습니다.
    기존 방식으론 LottoConstants GameConstants를 GameConstants를 선언만 하고 할당하지 않아 null이었습니다. (유틸클래스이기에)
    
    null인 필드를 참조하는 대신, import한 유틸 클래스(LottoConstants)의 static 변수를 직접 참조하도록 변경하였습니다.

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -4,15 +4,13 @@ import lotto.config.LottoConstants;
 
 public final class ErrorMessage {
 
-    private static LottoConstants GameConstants;
-
     private ErrorMessage() {
         throw new IllegalStateException("Utility class");
     }
 
     private static final String ERROR_PREFIX = "[ERROR] ";
 
-    public static final String INVALID_PURCHASE_AMOUNT_UNIT = ERROR_PREFIX + "구입 금액은 " + GameConstants.LOTTO_PRICE + "원 단위여야 합니다.";
+    public static final String INVALID_PURCHASE_AMOUNT_UNIT = ERROR_PREFIX + "구입 금액은 " + LottoConstants.LOTTO_PRICE + "원 단위여야 합니다.";
     public static final String INVALID_NUMBER_FORMAT = ERROR_PREFIX + "숫자를 입력해야 합니다.";
     public static final String CANNOT_BE_ZERO = "0은 입력할 수 없습니다.";
     public static final String INVALID_LOTTO_NUMERS_FORMART = ERROR_PREFIX + "양식은 숫자사이에 ,만 와야하며 공백은 불가합니다.";
