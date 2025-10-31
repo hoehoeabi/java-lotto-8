b4b2eaa494b2becc435409b3da3af0cef415cb15
hoehoeabi <0203jyh00@naver.com>
2025-10-31 22:53:58 +0900

    Docs : 생성자와 필드 사이 줄바꿈을 추가하였습니다.

--- src/main/java/lotto/message/InputMessage.java
+++ src/main/java/lotto/message/InputMessage.java
@@ -1,10 +1,13 @@
 package lotto.message;
 
 public final class InputMessage {
+
     private InputMessage() {
         throw new IllegalStateException("Utility class");
     }
+
     public static final String PROMPT_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
     public static final String PROMPT_WINNING_NUMBERS = "당첨 번호를 입력해 주세요.";
     public static final String PROMPT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
 }
--- src/main/java/lotto/message/OutputMessage.java
+++ src/main/java/lotto/message/OutputMessage.java
@@ -1,12 +1,15 @@
 package lotto.message;
 
 public final class OutputMessage {
+
     private OutputMessage() {
         throw new IllegalStateException("Utility class");
     }
+
     public static final String PURCHASE_COUNT_FORMAT = "\n%d개를 구매했습니다.";
     public static final String STATISTICS_HEADER = "\n당첨 통계\n---";
     public static final String STATISTICS_RESULT_FORMAT = "%d개 일치 (%s원) - %d개\n";
     public static final String STATISTICS_BONUS_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개\n";
     public static final String RATE_OF_RETURN_FORMAT = "총 수익률은 %.1f%%입니다.";
+
 }
